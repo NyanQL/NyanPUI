19ca7675288a8cdd581856280cc516a43275d05b
Takako Miyagawa <nekoget@gmail.com>
2025-03-03 10:23:00 +0900

    公開版作成

---
+++ javascript/html.js
@@ -0,0 +1,17 @@
+main();
+
+function main() {
+
+    console.log(nyanAllParams.api);
+    console.log("console.log:" + typeof console.log);
+    console.log("nyanHostExec:" + typeof nyanHostExec);
+    console.log("nyanGetAPI:" + typeof nyanGetAPI);
+    console.log("nyanJsonAPI:" + typeof nyanJsonAPI);
+    console.log("nyanGetItem:" + typeof nyanGetItem);
+    console.log("nyanSetItem:" + typeof nyanSetItem);
+    console.log("nyanSetCookie:" + typeof nyanSetCookie);
+    console.log("nyanGetCookie:" + typeof nyanGetCookie);
+
+    //htmlコードを読み込み表示をする。
+    return nyanHtmlCode;
+}
